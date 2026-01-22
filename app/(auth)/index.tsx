fc93c876933cdb808b7b2b1bf80d1283020071c6
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-22 13:37:01 +0530

    run fade and slide animations together for 1 second

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,5 +1,6 @@
+import { TabActions } from "@react-navigation/native";
 import { useRouter } from "expo-router";
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Animated } from "react-native";
 
 
@@ -8,4 +9,23 @@ export default function WelcomeScreen() {
 
     const fadeAnim = useRef(new Animated.Value(0)).current;
     const translateAnim = useRef(new Animated.Value(30)).current;
+
+
+    useEffect(() => {
+        Animated.parallel([
+            Animated.timing(fadeAnim, {
+                toValue: 1,
+                duration: 1000,
+                useNativeDriver: true,  // use GPU for smoother 60fps performance
+            }),
+
+            Animated.timing(translateAnim, {
+                toValue: 0,
+                duration: 1000,
+                useNativeDriver: true
+            }),
+
+        ]).start();
+
+    }, []);
 }
