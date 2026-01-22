2d8aabd8c5c0eae8e2aed1c2ff1b1817a8120f2f
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-22 13:32:27 +0530

    initializing animation values opacity and translateY

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,7 +1,11 @@
 import { useRouter } from "expo-router";
-import React from "react";
+import React, { useRef } from "react";
+import { Animated } from "react-native";
 
 
 export default function WelcomeScreen() {
     const router = useRouter();
+
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+    const translateAnim = useRef(new Animated.Value(30)).current;
 }
