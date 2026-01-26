c676bb4dca1003966e6506fd1af0bdde3aeb6bfd
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-26 19:03:08 +0530

    welcome screen and login + register screen

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,15 @@
+import { Stack } from "expo-router"
 
-
+// layout for authentication-related screens
 const AuthLayout = () => {
-    
-}
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+
+            <Stack.Screen name="index" />
+            <Stack.Screen name="loginRegister" />
+
+        </Stack>
+    );
+};
+
+export default AuthLayout;
