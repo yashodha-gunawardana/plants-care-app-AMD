8240a13f1391ef2380d97937ef0b2fea5adaaf36
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-26 11:39:54 +0530

    initialize firestore database

--- config/firebase.ts
+++ config/firebase.ts
@@ -25,4 +25,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(AsyncStorage),
 
-})
+});
+
+export const db = getFireStore(app);
