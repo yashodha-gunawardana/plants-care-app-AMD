11b2f42cda96ff07d3519c2c19f8dfeef86fff69
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-26 18:43:53 +0530

    main container for the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
+import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 const RootLayout = () => {
     const insets = useSafeAreaInsets();
+
+    return ( 
+        <View style={{ marginTop: insets.top, flex: 1 }}>
+
+        </View>
+    )
 }
