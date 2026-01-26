5315e05c07cb7883bc3bbb1052fa320956412f27
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-26 14:26:32 +0530

    create user account using email and password

--- services/authService.ts
+++ services/authService.ts
@@ -1,1 +1,18 @@
+import { auth } from "@/config/firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+
+
+
+export const registerUser = async (
+    fullname: string,
+    email: string,
+    password: string
+) => {
+    
+    const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+    )
+}
