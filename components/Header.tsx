6fb753b3d166849a29e7d367a31da616a86cd276
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-27 18:08:44 +0530

    converts route name to the readable title when press icon on the navigationbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,5 +3,25 @@ import { usePathname } from "expo-router"
 
 
 const DashboardHeader = () => {
+    // current route path
     const pathname = usePathname();
+
+    const getHeaderTitle = () => {
+        if (pathname.includes("home"))
+            return "My Garden";
+
+        if (pathname.includes("wiki"))
+            return "Plants Wiki";
+
+        if (pathname.includes("add"))
+            return "Add Plants";
+
+        if (pathname.includes("log"))
+            return "Water Log";
+
+        if (pathname.includes("settings"))
+            return "Settings";
+
+        return "Dashboard";
+    };
 }
