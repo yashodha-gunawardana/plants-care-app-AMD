f53c0b46ffb24b93a1c93c92a6076569627a919f
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-29 14:15:09 +0530

    safety check

--- services/plantService.ts
+++ services/plantService.ts
@@ -2,4 +2,8 @@ import { auth } from "@/config/firebase"
 
 export const createPlant = async (plantData: any) => {
     const user = auth.currentUser;
+
+    if (!user) {
+        throw new Error("User not authenticated..")
+    }
 }
