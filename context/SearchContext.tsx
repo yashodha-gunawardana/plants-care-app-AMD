cfdfa51a1b3d9d9ed6212cd737ba3fd617fd9fba
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-30 00:02:40 +0530

    create the search context

--- context/SearchContext.tsx
+++ context/SearchContext.tsx
@@ -1,6 +1,11 @@
+import { createContext } from "react";
 
 
+// define search contect
 type SearchContextType = {
     searchQuery: string; // store current search text
     setSearchQuery: (query: string) => void;  // update the search text
 }
+
+
+const SearchContext = createContext<SearchContextType | undefined>(undefined);
