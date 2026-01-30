da79f188d5268473c2856be3cf563cfa04a741c0
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-30 17:04:54 +0530

    run animation whenever isible changes

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Animated } from "react-native";
 
 
@@ -15,4 +15,39 @@ const Toast = ({ visible, message, type = "info" }: ToastProps) => {
     
     const opacity = useRef(new Animated.Value(0)).current;
     const translateY = useRef(new Animated.Value(20)).current;
+
+
+    useEffect(() => {
+        if (visible) {
+            // animate toast IN (fade + slide up)
+            Animated.parallel([
+                Animated.timing(opacity, {
+                    toValue: 1,
+                    duration: 250,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(translateY, {
+                    toValue: 0,
+                    duration: 250,
+                    useNativeDriver: true,
+                }),
+            ]).start();
+            
+        } else {
+
+            // animate toast OUT (fade + slide down)
+            Animated.parallel([
+                Animated.timing(opacity, {
+                    toValue: 0,
+                    duration: 200,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(translateY, {
+                    toValue: 20,
+                    duration: 200,
+                    useNativeDriver: true,
+                }),
+            ]).start();
+        }
+    }, [visible]);
 }
