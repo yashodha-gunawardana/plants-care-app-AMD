ec428cd20481ab83a7360f0ef7709a4bf7e41ca3
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-30 12:02:46 +0530

    structure for wiki item

--- app/(dashboard)/wiki.tsx
+++ app/(dashboard)/wiki.tsx
@@ -2,6 +2,16 @@ import { Platform, UIManager } from "react-native";
 
 
 
+// android animation setup
 if (Platform.OS === "android" && UIManager.setLayoutAnimationEnabledExperimental) {
     UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+
+type wikiItem = {
+    id: string;
+    category: string;
+    title: string;
+    description: string;
+    icon: string;
 }
