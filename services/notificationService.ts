af587b8650d5c36ef9fc532899812d0847f74309
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-31 13:19:13 +0530

    set how is notification should be

--- services/notificationService.ts
+++ services/notificationService.ts
@@ -2,5 +2,11 @@ import * as Notifications from "expo-notifications";
 
 
 Notifications.setNotificationHandler({
-    
-})
+    handleNotification: async ()=>  ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+        shouldShowBanner: true,
+        shouldShowList: true
+    }),
+});
