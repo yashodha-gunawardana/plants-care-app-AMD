93bfad0ab92fdcb523cd22abae0aa05f122b39b6
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-31 19:23:23 +0530

    reusable component for displaying individual care items

--- app/(modals)/plant-details.tsx
+++ app/(modals)/plant-details.tsx
@@ -1,4 +1,5 @@
 import { PlantContext } from "@/context/PlantContext";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useContext } from "react";
 import { Dimensions, StyleSheet, View, Text, Alert } from "react-native";
@@ -49,11 +50,43 @@ const PlantDetailsModal = () => {
         ]);
     };
 
-    
+
+    // reusable component for care items
+    const CareRow = ({ icon, label, value, isMaterial = false }: any) => (
+        <View style={styles.careRow}>
+            <View style={styles.careIconBox}>
+                {isMaterial ? (
+                    <MaterialCommunityIcons name={icon} size={24} color="#1A3C34" />
+                ) : (
+                    <Ionicons name={icon} size={24} color="#1A3C34" />
+                )}
+            </View>
+
+            <View style={styles.careTextContainer}>
+                <Text style={styles.careLabel}>{label}</Text>
+                <Text style={styles.careValue}>{value}</Text>
+            </View>
+        </View>
+    );
 }
 
 
 const styles = StyleSheet.create({
 
     center: { flex: 1, justifyContent: "center", alignItems: "center" },
+    careRow: { flexDirection: "row", alignItems: "center", marginBottom: 20 },
+
+    careIconBox: {
+        width: 58,
+        height: 58,
+        backgroundColor: "#F4F6F4",
+        borderRadius: 18,
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 18,
+    },
+
+    careTextContainer: { flex: 1 },
+    careLabel: { fontSize: 18, fontWeight: "800", color: "#1A3C34" },
+    careValue: { fontSize: 14, color: "#7A8A7A", marginTop: 2, fontWeight: "500" },
 })
