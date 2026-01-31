733a6533fd2118f0824f1719466dd52a19371503
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-02-01 00:19:18 +0530

    styles edit-plnat container

--- app/(modals)/edit-plant.tsx
+++ app/(modals)/edit-plant.tsx
@@ -174,10 +174,19 @@ const EditPlantModal = () => {
             </TouchableOpacity>
         );
     };
+
+
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
 }
 
 
 const styles = StyleSheet.create({
+
+    container: { flex: 1, backgroundColor: "#FBFCFB" },
 
     careRow: { 
         flexDirection: "row", 
