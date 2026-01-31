a85b97c98065cf7973df33d06f4a0b7783425b37
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-02-01 01:05:09 +0530

    'pan' tracks the X and Y animated coordinates

--- app/(dashboard)/home.tsx
+++ app/(dashboard)/home.tsx
@@ -1,4 +1,21 @@
-import { Dimensions } from "react-native";
+import { PlantContext } from "@/context/PlantContext";
+import { useRouter } from "expo-router";
+import { useContext, useRef } from "react";
+import { Dimensions, Animated } from "react-native";
 
 
+// get screen dimensions for initial placement of the draggable button
 const { width, height } = Dimensions.get("window");
+
+
+const HomeScreen = () => {
+    
+    const { plants, loading, fetchPlants } = useContext(PlantContext);
+    const router = useRouter();
+
+    // for the entry fade-in effect
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    // 'pan' tracks the X and Y animated coordinates
+    const pan = useRef(new Animated.ValueXY({ x: width - 85, y: height - 200 })).current;
+}
