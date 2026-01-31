aa7d3fb41fb8e2bd8b0ae640dd59b668328cc477
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-31 18:16:09 +0530

    helper function to render small status icons for watering, fertilizing, etc

--- components/PlantCard.tsx
+++ components/PlantCard.tsx
@@ -1,5 +1,5 @@
 import { Plant } from "@/context/PlantContext";
-
+import { useRouter } from "expo-router";
 
 
 interface PlantCardProps {
@@ -8,3 +8,15 @@ interface PlantCardProps {
 
 
 const DEFAULT_PLANT_IMAGE = "https://i.pinimg.com/1200x/9b/77/f6/9b77f61cdb7dffbd979b1d7b02cfa937.jpg";
+
+
+const PlantCard = ({ item }: PlantCardProps) => {
+    const router = useRouter();
+
+
+    const renderCareBadges = () => {
+        const schedules = item.careSchedules;
+
+        if (!schedules) return null;
+    }
+}
