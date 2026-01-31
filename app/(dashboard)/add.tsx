238a68443fdcaed51eb52931785fc936e4a09be1
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-31 13:56:52 +0530

    access the global plant context to save new data

--- app/(dashboard)/add.tsx
+++ app/(dashboard)/add.tsx
@@ -1,3 +1,6 @@
+import { PlantContext } from "@/context/PlantContext";
+import { useRouter } from "expo-router";
+import { useContext } from "react";
 import { Dimensions } from "react-native";
 
 
@@ -6,8 +9,16 @@ const { width } = Dimensions.get("window");
 type CareType = "watering" | "light" | "temp" | "fertilize" | "repot";
 
 
+// structure for a single care activity's schedule
 interface ScheduleConfig {
     interval: number;
     selectedDays: number[];
     selectedTime: string;
+}
+
+
+const AddPlantScreen = () => {
+    const router = useRouter();
+
+    const { addPlant } = useContext(PlantContext);
 }
