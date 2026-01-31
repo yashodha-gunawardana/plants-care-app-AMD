fe715b9fd7462705d55d2e89f2d11eaaa2051c1b
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-31 19:10:05 +0530

    access global plant state and the removal function from Context

--- app/(modals)/plant-details.tsx
+++ app/(modals)/plant-details.tsx
@@ -1,4 +1,6 @@
-import { useLocalSearchParams } from "expo-router";
+import { PlantContext } from "@/context/PlantContext";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useContext } from "react";
 import { Dimensions } from "react-native";
 
 
@@ -10,5 +12,10 @@ const DEFAULT_PLANT_IMAGE = "https://i.pinimg.com/736x/9b/77/f6/9b77f61cdb7dffbd
 
 const PlantDetailsModal = () => {
 
+    // retrieve the plant ID from the URL parameters
     const { id } = useLocalSearchParams<{ id: string }>();
+    const router = useRouter();
+
+    const { plants, removePlant } = useContext(PlantContext);
+
 }
