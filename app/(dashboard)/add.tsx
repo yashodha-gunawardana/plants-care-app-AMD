1acdb672a9e6cc4df88e1bb0462b02e31ad082f5
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-31 13:55:16 +0530

    structure for a single care activity's schedule

--- app/(dashboard)/add.tsx
+++ app/(dashboard)/add.tsx
@@ -3,5 +3,11 @@ import { Dimensions } from "react-native";
 
 const { width } = Dimensions.get("window");
 
-
 type CareType = "watering" | "light" | "temp" | "fertilize" | "repot";
+
+
+interface ScheduleConfig {
+    interval: number;
+    selectedDays: number[];
+    selectedTime: string;
+}
