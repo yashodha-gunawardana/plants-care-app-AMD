7586e6e1b23b85c41aeccbda876ad4b13fc127cc
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-02-08 13:02:01 +0530

    list of avaiable avatar identifiers stored in your cloudinary account

--- app/(dashboard)/settings.tsx
+++ app/(dashboard)/settings.tsx
@@ -5,3 +5,21 @@
 const CLOUD_NAME = "dnefdegz0";
 const CLOUDINARY_BASE_URL = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload`;
 const DEFAULT_AVATAR_ID = "default_placeholder_crn3se";
+
+const COLORS = {
+    forest: "#17402A",
+    white: "#FFFFFF",
+    muted: "#83918B",
+    brown: "#8D6E63",
+    lightRed: "#FFF1F1",
+    darkRed: "#D32F2F",
+    lightGray: "#F0F0F0",
+    golden: "#F4A261",
+};
+
+
+// list avatar 
+const AVATAR_PUBLIC_IDS: string[] = [
+    "male_2_rnpy7q", "male_1_mcnb1r", "female_3_ztizys",
+    "female_2_yuk22n", "female_1_bjjecv", "cute_1_mlthan",
+];
